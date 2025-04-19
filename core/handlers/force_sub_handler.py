5104c4904bb95050cda5112390ffb785862dc5c3
landanfp <159138257+landanfp@users.noreply.github.com>
2025-04-19 10:14:19 +0330

    Update force_sub_handler.py

--- core/handlers/force_sub_handler.py
+++ core/handlers/force_sub_handler.py
@@ -13,7 +13,7 @@ async def handle_force_subscribe(bot, cmd):
             await bot.send_message(
                 chat_id=cmd.from_user.id,
                 text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/linux_repo).",
-                parse_mode="markdown",
+                #parse_mode="markdown",
                 disable_web_page_preview=True
             )
             return 400
@@ -43,7 +43,7 @@ async def handle_force_subscribe(bot, cmd):
         await bot.send_message(
             chat_id=cmd.from_user.id,
             text="Something went Wrong. Contact my [Support Group](https://t.me/linux_repo).",
-            parse_mode="markdown",
+            #parse_mode="markdown",
             disable_web_page_preview=True
         )
         return 400
