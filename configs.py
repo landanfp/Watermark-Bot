01487e34f42e2d627faa7d021c7e072dbd56bd18
landanfp <159138257+landanfp@users.noreply.github.com>
2025-04-19 10:29:50 +0330

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,7 +4,7 @@ import os
 
 
 class Config(object):
-	BOT_TOKEN = os.environ.get("BOT_TOKEN","7136875110:AAFzyr2i2FbRrmst1sklkJPN7Yz2rXJvSew")
+	BOT_TOKEN = os.environ.get("BOT_TOKEN","6964975788:AAH3OrL9aXHuoIUliY6TJbKqTeR__X5p4H8")
 	API_ID = int(os.environ.get("API_ID", "3335796"))
 	API_HASH = os.environ.get("API_HASH","138b992a0e672e8346d8439c3f42ea78")
 	STREAMTAPE_API_PASS = os.environ.get("STREAMTAPE_API_PASS", "NoNeed")
@@ -15,7 +15,7 @@ class Config(object):
 	PRESET = os.environ.get("PRESET", "ultrafast")
 	OWNER_ID = int(os.environ.get("OWNER_ID", "763990585"))
 	CAPTION = "★ @FarshidBand ™"
-	BOT_USERNAME = os.environ.get("BOT_USERNAME", "ir_YoutubeDlbot")
+	BOT_USERNAME = os.environ.get("BOT_USERNAME", "ir_AhangDLBot")
 	DATABASE_URL = os.environ.get("DATABASE_URL","mongodb+srv://abirhasan2005:abirhasan@cluster0.i6qzp.mongodb.net/cluster0?retryWrites=true&w=majority")
 	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
 	ALLOW_UPLOAD_TO_STREAMTAPE = bool(os.environ.get("ALLOW_UPLOAD_TO_STREAMTAPE", True))
